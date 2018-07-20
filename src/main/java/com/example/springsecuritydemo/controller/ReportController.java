7ed97657cb539f27119d9d4fced8a309431871ed
kurnakov92 <kurnakov92@gmail.com>
2018-07-20 11:51:23 +0700

    some commit

--- src/main/java/com/example/springsecuritydemo/controller/ReportController.java
+++ src/main/java/com/example/springsecuritydemo/controller/ReportController.java
@@ -3,7 +3,10 @@ package com.example.springsecuritydemo.controller;
 import com.example.springsecuritydemo.service.ReportService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -12,11 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReportController {
 
     @Autowired
-    private ReportService service;
+    private ReportService reportService;
 
     @RequestMapping("demo")
-    public String demo(){
+    public String demo() {
         return "Hello";
+    }
+
+    @RequestMapping(value = "report.csv", method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public String getReportCsv(@RequestParam String filePath){
+        String result = "";
+        if (filePath != null) {
+
+        }
+        return result;
     }
 
 
